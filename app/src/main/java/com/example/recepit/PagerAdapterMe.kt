c4422044e1e395fcdabac5f7ad92ff63413376d0
Zay Phyo01 <zayphyo0099@gmail.com>
2019-06-14 19:02:03 +0630

    bug

--- app/src/main/java/com/example/recepit/PagerAdapterMe.kt
+++ app/src/main/java/com/example/recepit/PagerAdapterMe.kt
@@ -10,18 +10,34 @@ import android.view.View
 class PagerAdapterMe(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
 
     override fun getItem(p0: Int): Fragment {
-        var fg: Fragment? = InfoFragment()
+        val fg: Fragment
         if (p0 == 0)
             fg = InfoFragment()
-        return fg!!
+        else
+            if (p0 == 1) {
+                fg = MaterailFragment()
+            }
+        else {
+                fg = VideoFragment()
+            }
+        return fg
 
     }
 
-    override fun getCount(): Int{return 3}
+    override fun getCount(): Int {
+        return 3
+    }
 
     override fun getPageTitle(position: Int): CharSequence? {
-        return if (position == 0) { "Info" }else
-        { "info" }
+        val title : String
+        if (position == 0) {
+            title = "info"
+        } else
+            if (position == 1) {
+                title = "Materail & Doing"
+            } else
+                title = "Video"
+        return title
     }
 
 
